078b2d5aad7cfb4e5cf51c1209f9f766f8951ab5
Adam Ever-Hadani <adamhadani@gmail.com>
2012-09-05 18:08:15 -0700

    added base.html tmplate to app skeleton, additions to urls and views defaults

--- project-template-01/project_name/urls.py
+++ project-template-01/project_name/urls.py
@@ -1,8 +1,22 @@
-from django.conf.urls import patterns, include, url
-from django.contrib import admin
+from django.conf.urls.defaults import patterns, include, url
+from django.views.generic import TemplateView, RedirectView
+#from django.contrib.auth.views import login, logout
 
+
+from django.contrib import admin
+# To Use GeoDjango's derived admin module, enable instead:
+#from django.contrib.gis import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
+                       
+    # To enable admin app access
     url(r'^admin/', include(admin.site.urls)),
+    
+    # i18n / l10n
+    (r'^i18n/', include('django.conf.urls.i18n')),
+    
+    # Example to include app-specific urls
+    #url(r'^myapp/', include('myapp.urls')),
+    
 )
